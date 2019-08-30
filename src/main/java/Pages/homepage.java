61cacc69903af5ba36dacd9a3e231a1f6e25ea34
FDN-Prasetyo <prasetyo531@gmail.com>
2019-08-30 18:50:19 +0700

    sakd

--- src/main/java/Pages/homepage.java
+++ src/main/java/Pages/homepage.java
@@ -30,10 +30,10 @@ public class homepage {
 	@FindBy(linkText = "Reviews")
 	@CacheLookup
 	private WebElement reviewss;
-	
+
 	By homepage=By.xpath("//*[@id='__next']/div/div/div[1]/a");
 	By footerBrand=By.linkText("Brands");
-	
+
 	By joinletter=By.xpath("/html/body/div[4]/div/div/div[2]/p[2]/a");
 	By joinletterReviewPage=By.cssSelector("body > div:nth-child(6) > div > div > div.jsx-3828308350.modal-join-content-right > p:nth-child(3) > a");
 	By pointaddproduct=By.cssSelector("div[class='gbheader-add-area']");
@@ -45,19 +45,19 @@ public class homepage {
 	By menuHair=By.cssSelector("#__next > div > div > div.jsx-746878661.gtmenu > div.jsx-746878661.gtmenu-menu-main > div.jsx-746878661.gtmenu-menu-left > div > div > div:nth-child(1) > a");
 	By menuSkincare=By.cssSelector("#__next > div > div > div.jsx-746878661.gtmenu > div.jsx-746878661.gtmenu-menu-main > div.jsx-746878661.gtmenu-menu-left > div > div > div:nth-child(3) > a");
 	By findaddreview=By.cssSelector("#__next > div > div > div.jsx-1787593642.home-content > div.jsx-1787593642.home-column.margin-bottom-30 > div.jsx-1787593642.home-one-quarter > div.jsx-1787593642.home-reviews-content > button");
-	
+
 	public homepage(WebDriver driver) {
 		// TODO Auto-generated constructor stub
-		
+
 		this.driver=driver;
-		
+
 	}
-	
+
 	public WebElement closeTooltip(){
 
 		return driver.findElement(tooltip);
 	}
-	
+
 	public void Hamburger(){
 
 		this.wait.until(ExpectedConditions.elementToBeClickable(this.hamburger));
@@ -66,42 +66,42 @@ public class homepage {
 	}
 
 	public WebElement letmejoinletter(){
-		
+
 		return driver.findElement(joinletter);
 	}
 
 	public WebElement letmejoinletter2(){
-		
+
 		return driver.findElement(joinletterReviewPage);
 	}
-	
+
 	public WebElement clickMenuReview(){
-		
+
 		return driver.findElement(reviews);
 	}
-	
+
 	public WebElement getMenuBody(){
-		
+
 		return driver.findElement(menuBody);
 	}
-	
+
 	public WebElement getMenuHair(){
-		
+
 		return driver.findElement(menuHair);
 	}
-	
+
 	public WebElement getMenuSkincare(){
-		
+
 		return driver.findElement(menuSkincare);
 	}
-	
+
 	public WebElement getAddProduct(){
-		
+
 		return driver.findElement(pointaddproduct);
 	}
-	
+
 	public WebElement clickAddProduct(){
-		
+
 		return driver.findElement(clickaddproduct);
 	}
 
@@ -110,27 +110,27 @@ public class homepage {
 
 		return driver.findElement(clicklogin);
 	}
-	
+
 	//search
 	public WebElement clickSearch(){
-		
+
 		return driver.findElement(searchelement);
 	}
-	
+
 	public WebElement clickAddReviewHomepage(){
-		
+
 		return driver.findElement(findaddreview);
 	}
-	
+
 	public WebElement pointUserLoginHeader(){
-		
+
 		return driver.findElement(pointloginlogout);
 	}
-	
+
 	public WebElement clickBrandFooter(){
-		
+
 		return driver.findElement(footerBrand);
 	}
-	
+
 
 }
