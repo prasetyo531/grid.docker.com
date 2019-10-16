c7be9865a5b541182aa5444c598db6da143d87d3
maciahauna <maciahauna@gmail.com>
2019-10-16 18:17:02 +0700

    update

---
+++ src/main/java/Pages/notification.java
@@ -0,0 +1,42 @@
+package Pages;
+
+import AssertObject.assertHome;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class notification {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    assertHome assert = new assertHome(driver);
+
+    @FindBy(id = "id_tab_reviews")
+    @CacheLookup
+    private WebElement tabReviews;
+
+    @FindBy(id = "id_tab_talks")
+    @CacheLookup
+    private WebElement tabTalks;
+
+    @FindBy(css = "div[class='jsx-3708272525 notifications-btn-follow']")
+    @CacheLookup
+    private WebElement buttonFollow;
+
+    @FindBy(css = "div[class='jsx-3708272525 notifications-image-cover']")
+    @CacheLookup
+    private WebElement userProfile;
+
+    @FindBy(css = "span[class='jsx-3708272525 notifications-link']")
+    @CacheLookup
+    private WebElement userProfile1;
+}
