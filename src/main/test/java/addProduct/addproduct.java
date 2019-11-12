c8ad9da1ba8e44b8fd1c507248856d134b673316
FDN-Prasetyo <prasetyo531@gmail.com>
2019-11-12 16:45:29 +0700

    no message

--- src/main/test/java/addProduct/addproduct.java
+++ src/main/test/java/addProduct/addproduct.java
@@ -27,89 +27,89 @@ public class addproduct extends baseTest {
     /*
     fill all concern - photo - follow first user
      */
-        public static WebElement main = null;
-        public static Properties prop = null;
-        public String UrlLogin;
+    public static WebElement main = null;
+    public static Properties prop = null;
+    public String UrlLogin;
 
-        assertHome asser = new assertHome(driver);
-
-
-        @Test(dataProvider = "getDataRegister")
-        public void addProduct(String username, String password) throws InterruptedException, AWTException {
-
-            login logpro = new login(driver);
-            homepage home = new homepage(driver);
-            addProduct addNewProd = new addProduct(driver);
-
-            driver.get("https://femaledaily.com/");
-            driver.manage().window().maximize();
-
-            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-
-            logpro.clickBtnLoginUser();
-            logpro.fillusername().sendKeys(username);
-            logpro.fillpassword().sendKeys(password);
-            logpro.clickbuttonlogin();
-
-            System.out.println(driver.getTitle());
-            if (!driver.getTitle().equals("Female Daily - Info, Artikel, Video dan Review Seputar Kecantikan")) {
-                System.out.println("Test Passed");
-            } else {
-                System.out.println("Test Failed");
-                fail("Test Failed");
-            }
-
-                home.WaitHamburger();
-
-                home.ClickMenuReview();
-
-                home.HoverClickMenuAddProduct();
-
-                Thread.sleep(5000);
-
-                addNewProd.popUpAddProduct();
-
-                Thread.sleep(3000);
-
-                addNewProd.imagebyUpload();
-                //addNewProd.imagebyURL();
-                addNewProd.clickbuttonNext();
-
-                addNewProd.selectBrand();
-                addNewProd.selectProductCategory();
-                addNewProd.selectSubProductCategory();
-                addNewProd.selectSubCategory();
-                addNewProd.inputProductName();
-                addNewProd.inputProductShade();
-                addNewProd.clickbuttonNext1();
-
-                addNewProd.inputRating();
-                addNewProd.setProductPrice();
-                addNewProd.setPackageQuality();
-                addNewProd.setRepurchase();
-                addNewProd.inputReview();
-                addNewProd.clickbuttonNext2();
-
-                //addNewProd.selectCurrecy();
-                addNewProd.inputNominal();
-                addNewProd.productDescription();
-                addNewProd.tagsProduct();
-                addNewProd.clickbuttonSubmit();
-            }
+    assertHome asser = new assertHome(driver);
 
 
-            @DataProvider
-            public static Object[][] getDataRegister () {
-                Object[][] data = null;
+    @Test(dataProvider = "getDataRegister")
+    public void addProduct(String username, String password) throws InterruptedException, AWTException {
 
-                //kiri for numbers of times testcase must execute
-                //kanan for no parameter you send
-                data = new Object[1][2];
+        login logpro = new login(driver);
+        homepage home = new homepage(driver);
+        addProduct addNewProd = new addProduct(driver);
 
-                data[0][0] = "maciahauna";
-                data[0][1] = "hanamasa";
+        driver.get("https://femaledaily.com/");
+        driver.manage().window().maximize();
 
-                return data;
-            }
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+        logpro.clickBtnLoginUser();
+        logpro.fillusername().sendKeys(username);
+        logpro.fillpassword().sendKeys(password);
+        logpro.clickbuttonlogin();
+
+        System.out.println(driver.getTitle());
+        if (!driver.getTitle().equals("Female Daily - Info, Artikel, Video dan Review Seputar Kecantikan")) {
+            System.out.println("Test Passed");
+        } else {
+            System.out.println("Test Failed");
+            fail("Test Failed");
         }
+
+        home.WaitHamburger();
+
+        home.ClickMenuReview();
+
+        home.HoverClickMenuAddProduct();
+
+        Thread.sleep(5000);
+
+        addNewProd.popUpAddProduct();
+
+        Thread.sleep(3000);
+
+        addNewProd.imagebyUpload();
+        //addNewProd.imagebyURL();
+        addNewProd.clickbuttonNext();
+
+        addNewProd.selectBrand();
+        addNewProd.selectProductCategory();
+        addNewProd.selectSubProductCategory();
+        addNewProd.selectSubCategory();
+        addNewProd.inputProductName();
+        addNewProd.inputProductShade();
+        addNewProd.clickbuttonNext1();
+
+        addNewProd.inputRating();
+        addNewProd.setProductPrice();
+        addNewProd.setPackageQuality();
+        addNewProd.setRepurchase();
+        addNewProd.inputReview();
+        addNewProd.clickbuttonNext2();
+
+        //addNewProd.selectCurrecy();
+        addNewProd.inputNominal();
+        addNewProd.productDescription();
+        addNewProd.tagsProduct();
+        addNewProd.clickbuttonSubmit();
+    }
+
+
+    @DataProvider
+    public static Object[][] getDataRegister () {
+        Object[][] data = null;
+
+        //kiri for numbers of times testcase must execute
+        //kanan for no parameter you send
+        data = new Object[1][2];
+
+        data[0][0] = "maciahauna";
+        data[0][1] = "hanamasa";
+
+        return data;
+    }
+
+}
